afedd8ea989607386c9f764c1e4d5b2a1caadbf2
HaoZeke <driger633@gmail.com>
2017-06-27 05:54:43 +0530

    prog: Add omp to get_pot_reeg and add MPI subrot

--- src/Vreco_CPMD.f90
+++ src/Vreco_CPMD.f90
@@ -31,7 +31,6 @@ MODULE pot_data
 CONTAINS
 !    
   SUBROUTINE get_potential(so,scoll,is_torsion,scal,ds,s,w,prnt_dyn,ndyn,tmp,v)
-    USE omp_lib
     IMPLICIT NONE
 
     ! arguments
@@ -138,7 +137,7 @@ CONTAINS
 !
 !
   SUBROUTINE get_potential_reg(so,scoll,is_torsion,scal,ds,s,w,v)
-      
+    USE omp_lib  
     IMPLICIT NONE
 
     ! arguments
@@ -966,3 +965,115 @@ WRITE(*,'(/,a,a,/)')'Last line read is:',pfmt0
 STOP
 !
 END PROGRAM Vreco
+
+
+!****************************************************************************************!
+
+SUBROUTINE MPI_Start()
+IMPLICIT NONE
+INCLUDE 'mpif.h'
+INTEGER :: i_err
+#if defined (_PARALLEL)
+  call MPI_INIT(i_err)
+#endif
+END
+!****************************************************************************************!
+
+SUBROUTINE MPI_Stop()
+IMPLICIT NONE
+INCLUDE 'mpif.h'
+INTEGER :: i_err
+#if defined (_PARALLEL)
+call MPI_FINALIZE(i_err)
+#endif
+END
+!****************************************************************************************!
+
+SUBROUTINE get_ncpu(ncpu)
+IMPLICIT NONE
+INCLUDE 'mpif.h'
+INTEGER :: ncpu, i_err
+ncpu=1
+#if defined (_PARALLEL)
+call MPI_COMM_SIZE(MPI_COMM_WORLD,ncpu,i_err)
+#endif
+END
+!****************************************************************************************!
+
+SUBROUTINE get_cpuid(icpu)
+IMPLICIT NONE
+INCLUDE 'mpif.h'
+INTEGER :: icpu, i_err
+icpu=0
+#if defined (_PARALLEL)
+call MPI_COMM_RANK(MPI_COMM_WORLD,icpu,i_err)
+#endif
+END
+!****************************************************************************************!
+
+SUBROUTINE IBcast(myint,leng)
+IMPLICIT NONE
+INCLUDE 'mpif.h'
+INTEGER :: leng, myint(*), i_err
+#if defined (_PARALLEL)
+CALL MPI_BCAST(myint,leng,MPI_INTEGER,0,MPI_COMM_WORLD,i_err)
+#endif
+END
+!****************************************************************************************!
+
+SUBROUTINE RBcast(myreal,leng)
+IMPLICIT NONE
+INCLUDE 'mpif.h'
+INTEGER :: myreal(*)
+INTEGER :: leng, i_err
+#if defined (_PARALLEL)
+CALL MPI_BCAST(myreal,leng,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,i_err)
+#endif
+END
+!****************************************************************************************!
+
+SUBROUTINE Sync_Procs
+IMPLICIT NONE
+INCLUDE 'mpif.h'
+INTEGER i_err
+#if defined (_PARALLEL)
+call MPI_Barrier(MPI_COMM_WORLD,i_err)
+#endif
+END
+!****************************************************************************************!
+
+SUBROUTINE Set_Parent(parent)
+IMPLICIT NONE
+INCLUDE 'mpif.h'
+LOGICAL :: parent
+INTEGER :: icpu, i_err
+parent=.false.
+icpu=0
+#if defined (_PARALLEL)
+CALL MPI_COMM_RANK(MPI_COMM_WORLD,icpu,i_err)
+#endif
+IF(icpu.eq.0)parent=.true.
+END
+!****************************************************************************************!
+
+SUBROUTINE GlobSumR(myreal,leng)
+IMPLICIT NONE
+REAL*8 :: myreal(*)
+INTEGER :: leng,i_err
+INCLUDE 'mpif.h'
+#if defined (_PARALLEL)
+call MPI_Allreduce(myreal,myreal,leng,MPI_INTEGER,MPI_SUM,MPI_COMM_WORLD,i_err)
+#endif
+END
+!****************************************************************************************!
+
+SUBROUTINE GlobSumI(myint,leng)
+IMPLICIT NONE
+INTEGER :: myint(*)
+INTEGER :: leng,i_err
+INCLUDE 'mpif.h'
+#if defined (_PARALLEL)
+call MPI_Allreduce(myint,myint,leng,MPI_INTEGER,MPI_SUM,MPI_COMM_WORLD,i_err)
+#endif
+END
+!****************************************************************************************!
